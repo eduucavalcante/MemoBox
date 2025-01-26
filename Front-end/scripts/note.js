0864215fd8a36992523ef47c8b5e59d7b9cd108f
Eduardo Cavalcante <eduardo.cavalcante.contact@gmail.com>
2025-01-26 14:25:40 -0300

    Adicionando delete de notas no front-end

--- Front-end/scripts/note.js
+++ Front-end/scripts/note.js
@@ -17,12 +17,28 @@ async function fetchNoteDetails() {
             <p>${note.content}</p>
             <small>${note.createdAt}</small>
             <br>
-            <button>Excluir nota</button>
+            <button onclick="deleteNote(${note.id})">Excluir nota</button>
         `;
         noteContainer.appendChild(noteElement);
     } catch(error) {
         noteContainer.innerHTML = 'Erro ao carregar detalhes da nota';
         console.log(`Erro ao carregar nota: ${error}`);
+    }
+}
+
+async function deleteNote(id) {
+    const confirmDelete = confirm("Tem certeza que quer excluir a nota?");
+
+    if(!confirmDelete) {
+        return;
+    }
+
+    try  {
+        await axios.delete(`${API_URL}/${folderName}/${id}`);
+        window.location.href = `/folder.html?name=${folderName}`;
+    } catch (error) {
+        alert("Erro ao excluir nota.");
+        console.log(`Erro ao excluir a nota: ${error}`);
     }
 }
 
