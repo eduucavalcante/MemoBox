bfa8e210e6b137719e9dec9316bff6e0d869dcb4
Eduardo Cavalcante <eduardo.cavalcante.contact@gmail.com>
2025-01-26 12:29:13 -0300

    Adicionando funcionalidade de salvar notas dentro de pastas no front-end

--- Front-end/scripts/add.js
+++ Front-end/scripts/add.js
@@ -1,6 +1,28 @@
+const API_URL = 'http://localhost:8081';
+
 const urlParams = new URLSearchParams(window.location.search);
 const folderName = urlParams.get("folder");
 
-const h1 = document.querySelector('h1');
+const form = document.querySelector('form');
+const titleInput = document.querySelector('input');
+const contentInput = document.querySelector('textarea');
+document.documentElement.style.setProperty('--folder', `"na pasta ${folderName}"`);
 
-h1.innerText = folderName;
+async function addNote(event) {
+    event.preventDefault();
+
+    const newNote = {
+        title: titleInput.value,
+        content: contentInput.value
+    };
+
+    try {
+        await axios.post(`${API_URL}/${folderName}`, newNote);
+        window.location.href = `folder.html?name=${folderName}`;
+    } catch(error) {
+        alert("Erro ao salvar nota.");
+        console.log(`Erro ao salvar nota: ${error}`);
+    }
+}
+
+form.addEventListener("submit", addNote);
