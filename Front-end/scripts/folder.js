dc8dfbaeefce8d3b7460deeb42d42d4aeab3a795
Eduardo Cavalcante <eduardo.cavalcante.contact@gmail.com>
2025-01-28 13:42:05 -0300

    Adicionando exclusão de nota dentro da pasta no front-end

--- Front-end/scripts/folder.js
+++ Front-end/scripts/folder.js
@@ -8,11 +8,11 @@ newNoteBtn.addEventListener("click", () => {
     window.location.href = `/add.html?folder=${h1.innerText}`;
 });
 
+const urlParams = new URLSearchParams(window.location.search);
+const folderName = urlParams.get("name");
+
 async function fetchNotes() {
     notesContainer.innerHTML = '';
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const folderName = urlParams.get("name");
 
     h1.innerText = folderName;
 
@@ -29,6 +29,9 @@ async function fetchNotes() {
             const noteElement = document.createElement("div");
             noteElement.innerHTML = `
                 <a href="/note.html?name=${folderName}&id=${note.id}">${note.title}</a>
+                <div class="deleteDiv">
+                    <button class="deleteBtn" onclick="deleteNote(${note.id})"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/filled-trash.png" alt="filled-trash"/></button>
+                </div>
                 <hr>
             `;
             notesContainer.appendChild(noteElement);
@@ -36,6 +39,22 @@ async function fetchNotes() {
     } catch(error) {
         notesContainer.innerHTML = '<p>Erro ao carregar as notas...';
         console.log(`Erro ao carregar notas: ${error}`);
+    }
+}
+
+async function deleteNote(id) {
+    const confirmDelete = confirm("Tem certeza que quer excluir a nota?");
+
+    if(!confirmDelete) {
+        return;
+    }
+
+    try  {
+        await axios.delete(`${API_URL}/${folderName}/${id}`);
+        fetchNotes();
+    } catch (error) {
+        alert("Erro ao excluir nota.");
+        console.log(`Erro ao excluir a nota: ${error}`);
     }
 }
 
