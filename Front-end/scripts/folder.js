22a0cd56ded9736a042de837137fde0ee571c90e
Eduardo Cavalcante <eduardo.cavalcante.contact@gmail.com>
2025-01-30 17:38:55 -0300

    Adicionando delete de pastas no front-end

--- Front-end/scripts/folder.js
+++ Front-end/scripts/folder.js
@@ -3,10 +3,7 @@ const API_URL = 'http://localhost:8081';
 const notesContainer = document.querySelector('.notesContainer');
 const h1 = document.querySelector('h1');
 const newNoteBtn = document.querySelector('#newNote');
-
-newNoteBtn.addEventListener("click", () => {
-    window.location.href = `/add.html?folder=${h1.innerText}`;
-});
+const delFolderBtn = document.querySelector("#deleteFolder");
 
 const urlParams = new URLSearchParams(window.location.search);
 const folderName = urlParams.get("name");
@@ -57,5 +54,29 @@ async function deleteNote(id) {
         console.log(`Erro ao excluir a nota: ${error}`);
     }
 }
+
+async function deleteFolder(name) {
+    const confirmDelete = confirm("Tem certeza que quer excluir a pasta?");
+
+    if(!confirmDelete) {
+        return;
+    }
+
+    try {
+        await axios.delete(`${API_URL}/${name}`);
+        window.location.href = "/index.html";
+    } catch(error) {
+        alert("Erro ao excluir pasta.");
+        console.log(`Erro ao excluir pasta: ${error}`);
+    }
+}
+
+newNoteBtn.addEventListener("click", () => {
+    window.location.href = `/add.html?folder=${h1.innerText}`;
+});
+
+delFolderBtn.addEventListener("click", () => {
+    deleteFolder(folderName);
+});
 
 fetchNotes();
