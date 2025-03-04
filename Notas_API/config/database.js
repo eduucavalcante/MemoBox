ba63a0829112c14d29dafb0cd3e0c7d2ba2210f3
Eduardo Cavalcante <eduardo.cavalcante.contact@gmail.com>
2025-03-04 19:01:12 -0300

    Adicionando banco de dados remoto

--- Notas_API/config/database.js
+++ Notas_API/config/database.js
@@ -5,22 +5,17 @@ import path from 'path';
 
 dotenv.config();
 
-// const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-//     host: process.env.HOST,
-//     port: process.env.PORT,
-//     dialect: process.env.DIALECT,
-//     dialectOptions: {
-//         ssl: {
-//             rejectUnauthorized: true,
-//             ca: fs.readFileSync(process.env.CA_PATH)
-//         }
-//     },
-//     logging: false
-// });
-
-const sequelize = new Sequelize('notas', 'root', '040379', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    dialect: process.env.DIALECT,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,
+            ca: fs.readFileSync(process.env.CA_PATH)
+        }
+    },
+    logging: false
 });
 
 export default sequelize;
